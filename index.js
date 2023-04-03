016469566298cd3325f100170a54ef019086ee33
Renja <renjaschmakeit@gmail.com>
2023-04-03 13:10:24 +0200

    Deleted comments

--- index.js
+++ index.js
@@ -6,25 +6,7 @@ const dinoNames = ["Cayden", "Sauri", "Ernest", "Jarrett", "Tiffani", "Dusty", "
 const dinoGender = ["Male", "Female", "Genderfluid"]
 const textSnips = ["Your favorite Dinosaur is a ", "You get eaten by a ", "You get stomped on by a ", "You tamed a "];
 const allDinos = herbivores.concat(carnivores);
-// console.log(carnivores.length);
-
-// function randomDate(start, end) {
-//     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
-//   }
-  
-//   console.log(randomDate(new Date(2012, 0, 1), new Date()))
-
-// function getRandomDate() {
-//     const maxDate = Date.now();
-//     const timestamp =
-// }
-
-// Get random Number 
-// function randomNum() {
-//     let randomNum = Math.random()*10
-//     randomNum = Math.ceil(randomNum);
-//     return randomNum
-// }
+// console.log(carnivores.length); FOR DEBUGGING
 
 function randomNum(max) {
     max = Math.floor(max);
