03e3c8fcf9a954ef66d3c262ab548dc42c22b5eb
Renja <renjaschmakeit@gmail.com>
2023-04-03 11:28:23 +0200

    Added arrays, created random Number function

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello, test")
+"use strict";
+
+console.log("Hello, test");
+
+const herbivores = ["Ankylosaur", "Brontosaur", "Parasaur", "Diplodocus", "Paracer", "Iguanodon", "Beelzebufo", "Dodo"];
+const carnivores = ["T-Rex", "Raptor", "Carnotaurus", "Daeodon", "Direwolf", "Giganotosaur", "Hyaenodon", "Terror Bird"];
+const dinoNames = ["Cayden", "Sauri", "Ernest", "Jarrett", "Tiffani", "Dusty", "Rowina", "Lorin", "Blanche"]
+const dinoGender = ["Male", "Female", "Genderfluid"]
+const textSnips = ["Your favorite Dinosaur is ", "You get eaten by a ", "You get stomped on by a ", "You tamed a "];
+// console.log(carnivores.length);
+
+// function randomDate(start, end) {
+//     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+//   }
+  
+//   console.log(randomDate(new Date(2012, 0, 1), new Date()))
+
+// function getRandomDate() {
+//     const maxDate = Date.now();
+//     const timestamp =
+// }
+
+// Get random Number 
+// function randomNum() {
+//     let randomNum = Math.random()*10
+//     randomNum = Math.ceil(randomNum);
+//     return randomNum
+// }
+
+function randomNum(max) {
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max+1) + 0)
+}
+
+console.log(dinoNames.length);
+console.log(randomNum(dinoNames.length));
