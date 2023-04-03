f364e383a4bd08ffa8408821449a5e2f10b901ce
Renja <renjaschmakeit@gmail.com>
2023-04-03 12:26:49 +0200

    Completed the exercise. Added learnings to txt file.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-"use strict";
-
-console.log("Hello, test");
+console.log("Hello there!");
 
 const herbivores = ["Ankylosaur", "Brontosaur", "Parasaur", "Diplodocus", "Paracer", "Iguanodon", "Beelzebufo", "Dodo"];
 const carnivores = ["T-Rex", "Raptor", "Carnotaurus", "Daeodon", "Direwolf", "Giganotosaur", "Hyaenodon", "Terror Bird"];
 const dinoNames = ["Cayden", "Sauri", "Ernest", "Jarrett", "Tiffani", "Dusty", "Rowina", "Lorin", "Blanche"]
 const dinoGender = ["Male", "Female", "Genderfluid"]
-const textSnips = ["Your favorite Dinosaur is ", "You get eaten by a ", "You get stomped on by a ", "You tamed a "];
+const textSnips = ["Your favorite Dinosaur is a ", "You get eaten by a ", "You get stomped on by a ", "You tamed a "];
+const allDinos = herbivores.concat(carnivores);
 // console.log(carnivores.length);
 
 // function randomDate(start, end) {
@@ -29,22 +28,41 @@ const textSnips = ["Your favorite Dinosaur is ", "You get eaten by a ", "You get
 
 function randomNum(max) {
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max+1) + 0)
+    return Math.floor(Math.random() * (max))
 }
 
 function textAssembly() {
-    let snipPos = 0; //randomNum(textSnips.length);
+    let snipPos = randomNum(textSnips.length);
+    // console.log(snipPos); FOR DEBUGGING
     switch (snipPos) {
         case 0:
-            const allDinos = herbivores.concat(carnivores);
+            // let allDinos = herbivores.concat(carnivores);
             let dinoPos = randomNum(allDinos.length);
-            // console.log("DINOPOS: " + dinoPos);
-            console.log(textSnips[snipPos] + allDinos[dinoPos]);
+            // console.log("DINOPOS: " + dinoPos); FOR DEBUGGING
+            console.log(textSnips[snipPos] + allDinos[dinoPos] + ".");
             break;
+
+        case 1:
+            let carniPos = randomNum(carnivores.length);
+            console.log(textSnips[snipPos] + carnivores[carniPos] + ".");
+            break;
+        
+        case 2:
+            let herbiPos = randomNum(herbivores.length);
+            console.log(textSnips[snipPos] + herbivores[herbiPos] + ".");
+            break;
+        
+        case 3: 
+            // let allDinos = herbivores.concat(carnivores);
+            let tamedPos = randomNum(allDinos.length);
+            let namedPos = randomNum(dinoNames.length);
+            let genderPos = randomNum(dinoGender.length);
+            console.log(`${textSnips[snipPos]}${allDinos[tamedPos]} and named it ${dinoNames[namedPos]}. ${dinoNames[namedPos]}'s gender is ${dinoGender[genderPos]}.`)
+            break;
+
+        default: 
+            console.log("Something went wrong, sorry...")
     }
 }
 
 textAssembly();
-
-// console.log(dinoNames.length);
-// console.log(randomNum(dinoNames.length));
